26b08716169fd8e8c68f6a4d5998589c6c1718a0
sumukshashidhar <sumukuuu@gmail.com>
2020-11-10 12:01:54 +0530

    added sql connection stuff

--- src/frontend/sql_con.py
+++ src/frontend/sql_con.py
@@ -1,4 +1,13 @@
 
 def verify(username, password, dob):
+    """Perform SQL verification here
+
+    Args:
+        username (string): Name of the Person in FirstName<Space>Lastname
+        password (string): Password of the User
+        dob (string): Date of Birth as YYYY-MM-DD
+
+    Returns:
+        Boolean: Whether the check succeeded or not
+    """
     return True
-    
