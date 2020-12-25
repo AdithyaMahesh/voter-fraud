3081956d1f77f5964531021965cfcd7b70efe57f
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-12-26 02:37:40 +0530

    Completed update info functionality, with db connection and necessary rendering and redirection

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -98,6 +98,7 @@ def display():
 		return render_template("display.html", account = account) 
 	return redirect(url_for('login'))
 
+@app.route("/update", methods =['GET', 'POST']) 
 def update(): 
 	msg = '' 
 	if 'loggedin' in session: 
@@ -114,7 +115,20 @@ def update():
 			cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
 			cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, )) 
 			account = cursor.fetchone() 
-
+			if account: 
+				msg = 'Account already exists !'
+			elif not re.match(r'[^@]+@[^@]+\.[^@]+', email): 
+				msg = 'Invalid email address !'
+			elif not re.match(r'[A-Za-z0-9]+', username): 
+				msg = 'name must contain only characters and numbers !'
+			else: 
+				cursor.execute('UPDATE accounts SET username =% s, password =% s, email =% s, organisation =% s, address =% s, city =% s, state =% s, country =% s, postalcode =% s WHERE id =% s', (username, password, email, organisation, address, city, state, country, postalcode, (session['id'], ), )) 
+				mysql.connection.commit() 
+				msg = 'You have successfully updated !'
+		elif request.method == 'POST': 
+			msg = 'Please fill out the form !'
+		return render_template("update.html", msg = msg) 
+	return redirect(url_for('login'))
 
 
 
