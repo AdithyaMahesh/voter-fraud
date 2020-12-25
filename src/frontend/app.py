0abe8a80cdc7327dd15e3e8e651b62cad758ddd8
AdithyaMahesh <adithyamahesh2003@gmail.com>
2020-12-26 02:27:42 +0530

    Directed logout functionality using session variables

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -43,6 +43,14 @@ def login():
 			msg = 'Incorrect username / password !'
 	return render_template('login.html', msg = msg)
 
+
+@app.route('/logout') 
+def logout(): 
+    session.pop('loggedin', None) 
+    session.pop('id', None) 
+    session.pop('username', None) 
+    return redirect(url_for('login'))
+
     
 
 CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8000"
